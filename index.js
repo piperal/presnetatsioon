fd10df758695afa15b5f82a71bf44cc74655928b
piperal <piperal@tlu.ee>
2026-09-10 14:11:44 +0300

    .

---
+++ index.js
@@ -0,0 +1,20 @@
+async function getProducts() {
+    
+    const itemNum  = 1
+
+    try {
+        const res = await fetch(`https://fakestoreapi.com/products/${itemNum}`);
+
+        if (!res.ok) {
+            throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+
+        const json = await res.json();
+
+        console.log(json);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+getProducts();
